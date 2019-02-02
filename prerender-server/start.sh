2d662421b4fb36752836425275664a7999e2e4e0
Nadav Ivgi <nadav@shesek.info>
2019-02-02 23:13:31 +0200

    Accept list of flavors to enable for pre-renderer server

--- prerender-server/start.sh
+++ prerender-server/start.sh
@@ -2,7 +2,14 @@
 
 [[ -d node_modules ]] || npm install
 
-file=${1:-server}
+if [ "$1" == "--cluster" ]; then
+  file=cluster
+  shift
+else
+  file=server
+fi
+
+for flavor in "$@"; do source ../flavors/$flavor/config.env; done
 
 if [ -d dist ] && [ "$NODE_ENV" != "development" ]; then
   node dist/$file.js "$@"
