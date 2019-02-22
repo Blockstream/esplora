9526a3730aa7ab187683ba5d39b2b43ee32b39fb
Nadav Ivgi <nadav@shesek.info>
2019-02-22 02:32:59 +0200

    l10n: Update json2po and po2json to use flattened plural forms

--- lang/json2po.js
+++ lang/json2po.js
@@ -1,16 +1,38 @@
-#!/usr/bin/env
-const { readFileSync } = require('fs')
+#!/usr/bin/env node
+
+const fs = require('fs')
+    , path = require('path')
 
 const escape = str => !str ? '' : str.replace(/[\\"]/g, "\\$&")
 
-const tran = JSON.parse(readFileSync('/dev/stdin'))
-    , tranEn = require(`./en.json`)
+const lang_strs = JSON.parse(fs.readFileSync('/dev/stdin'))
+    , lang_id = lang_strs.lang_id
+    , eng_strs = require('./en.json')
 
-const poStr = readFileSync("./strings.txt").toString('utf8').split("\n").filter(Boolean).map(str => {
-  const val = tran[str] || tranEn[str] || str
-  return Array.isArray(val) ? `msgid "${str}"\nmsgid_plural "${str}__plural"\n`
-                                + val.slice(1).map((v, i) => `msgstr[${i}] "${escape(v || '-')}"`).join("\n")
-                            : `msgid "${str}"\nmsgstr "${escape(val)}"`
-}).join("\n")
+const poStr = fs.readFileSync(path.join(__dirname, 'strings.txt')).toString('utf8')
+  .split("\n")
+  .filter(Boolean)
+  .map(str => {
 
-console.log(poStr)
+    const has_plural = Array.isArray(eng_strs[str])
+    let lang_str = lang_strs[str] || (lang_id == 'en' && str)
+
+    if (has_plural && !Array.isArray(lang_str)) {
+      lang_str = [ lang_str, lang_str ]
+    }
+
+    return !lang_str ? null
+         : !has_plural ? `msgid "${str}"\nmsgstr "${escape(lang_str)}"`
+         : `msgid "${escape(str)} (singular)"\nmsgstr "${escape(lang_str[0])}"\n\n`
+          +`msgid "${escape(str)} (plural)"\nmsgstr "${escape(lang_str[1])}"`
+  })
+  .filter(Boolean)
+  .join("\n\n")
+
+console.log(`
+msgid ""
+msgstr ""
+"Language: ${lang_id}\\n"
+
+${poStr}
+`)
--- lang/po2json.js
+++ lang/po2json.js
@@ -1,10 +1,25 @@
-#!/usr/bin/env
+#!/usr/bin/env node
+
+const reSingular = / \(singular\)$/
+    , rePlural   = / \(plural\)$/
+
 require('pofile').load('/dev/stdin', (err, po) => {
   if (err) throw err
 
   const tran = po.items.filter(item => item.msgstr[0] != '' || item.msgstr.length > 1)
-    .reduce((T, { msgid, msgid_plural, msgstr }) =>
-      ({ ...T, [msgid]: msgid_plural ? [ null, ...msgstr ] : msgstr[0] }), {})
+    .reduce((T, { msgid, msgstr }) => {
+      if (msgstr.length) {
+        const isSingular = reSingular.test(msgid), isPlural = rePlural.test(msgid)
+        if (isSingular || isPlural) {
+          const msgid_s = msgid.replace(reSingular, '').replace(rePlural, '')
+          T[msgid_s] || (T[msgid_s] = [])
+          T[msgid_s][isSingular?0:1] = msgstr[0]
+        } else {
+          T[msgid] = msgstr[0]
+        }
+      }
+      return T
+    }, {})
 
   console.log(JSON.stringify(tran, null, 2))
 })
