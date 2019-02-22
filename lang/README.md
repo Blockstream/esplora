946459d26462dc010ab9f20898991459d9f36fc9
Nadav Ivgi <nadav@shesek.info>
2019-02-22 02:58:16 +0200

    l10n: Add "npm run fetch-transfiex" script to update all translation files

---
+++ lang/README.md
@@ -0,0 +1,5 @@
+The `.json` files in this directory are auto-generated from the `.po` files
+using the `build-json.sh` utility script. Please do not edit `.json` files
+directly.
+
+To contribute translations, please visit https://www.transifex.com/blockstream/esplora
---
+++ lang/fetch-transifex.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eo pipefail
+
+TRANSIFEX_PROJECT=esplora
+TRANSIFEX_RESOURCE=esplora
+
+for file in lang/*.po; do
+  lang=`basename $file`
+  lang=${lang%.*}
+  lang=${lang%-*}
+
+  # These are identified differently by transifex
+  [ "$lang" == "me" ] && lang=sr_ME
+  [ "$lang" == "jp" ] && lang=ja
+
+  echo "Downloading $lang from transifex to $file"
+  curl -s -L -u api:$TRANSIFEX_KEY https://www.transifex.com/api/2/project/$TRANSIFEX_PROJECT/resource/$TRANSIFEX_RESOURCE/translation/$lang?file=po > $file
+
+  echo "Generating json from $file"
+  ./lang/po2json.js < $file > ${file%.*}.json
+done
