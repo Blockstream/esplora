3fe1d1d13161eea7678100d8e6cc68c50f1ea141
Nadav Ivgi <nadav@shesek.info>
2019-02-22 02:55:22 +0200

    l10n: Add build-all-po to convert all .json files to .po, check-in the generated .po files

---
+++ lang/build-all-po.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script is only meant to be run once, initially.
+# After that, the canonical files are the .po files and the
+# .json files should no longer be updated.
+
+for file in lang/*.json; do
+  ./lang/json2po.js < $file > ${file%.*}.po
+done
