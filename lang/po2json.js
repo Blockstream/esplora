0b7a9d24ee83c24690412d0af944cc956baed171
Nadav Ivgi <nadav@shesek.info>
2019-02-22 07:41:20 +0200

    l10n: Optimize size by skipping translation strings that match the msgid

--- lang/po2json.js
+++ lang/po2json.js
@@ -8,7 +8,7 @@ require('pofile').load('/dev/stdin', (err, po) => {
 
   const tran = po.items.filter(item => item.msgstr[0] != '' || item.msgstr.length > 1)
     .reduce((T, { msgid, msgstr }) => {
-      if (msgstr.length) {
+      if (msgstr.length && !(msgstr.length == 1 && msgid === msgstr[0])) {
         const isSingular = reSingular.test(msgid), isPlural = rePlural.test(msgid)
         if (isSingular || isPlural) {
           const msgid_s = msgid.replace(reSingular, '').replace(rePlural, '')
