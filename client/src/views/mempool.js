3fd69044008add6d16acc220d03acec4ceecf35b
Nadav Ivgi <nadav@shesek.info>
2019-02-26 02:05:20 +0200

    Add mempool backlog statistics page

---
+++ client/src/views/mempool.js
@@ -0,0 +1,33 @@
+import Snabbdom from 'snabbdom-pragma'
+import { formatAmount } from './util'
+import layout from './layout'
+import search from './search'
+
+export default ({ t, mempool, feeEst, ...S }) => mempool && feeEst && layout(
+  <div>
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        { search({ t, klass: 'page-search-bar' }) }
+        <div>
+          <h1 className="block-header-title">{t`Mempool`}</h1>
+        </div>
+      </div>
+    </div>
+    <div className="container">
+      <div className="block-stats-table">
+        <div>
+          <div>{t`Total transactions`}</div>
+          <div>{mempool.count}</div>
+        </div>
+        <div>
+          <div>{t`Total fees`}</div>
+          <div>{formatAmount({ value: mempool.total_fee })}</div>
+        </div>
+        <div>
+          <div>{t`Total size (vKB)`}</div>
+          <div>{(mempool.vsize / 1000).toFixed(2)}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+, { ...S, t, mempool, feeEst })
