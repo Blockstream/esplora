38b27256d1a4c2ba44871bd88c9120edf9da6524
Nadav Ivgi <nadav@shesek.info>
2019-02-27 02:05:16 +0200

    Fix getMempoolDepth() for transactions paying over the maximum feerate in the histogram

--- client/src/views/util.js
+++ client/src/views/util.js
@@ -30,7 +30,7 @@ export const addressQR = addr => qruri(`bitcoin:${addr}`, { margin: 2 })
 
 export const getMempoolDepth = (fee_histogram, feerate) => {
   let depth = 0
-  for (let i=0; fee_histogram[i][0] > feerate; depth += fee_histogram[i++][1]);
+  for (let i=0; i < fee_histogram.length && fee_histogram[i][0] > feerate; depth += fee_histogram[i++][1]);
   return depth
 }
 
