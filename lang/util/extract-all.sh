269637fbc35caa2580be937e11fcfb53b7119241
Nadav Ivgi <nadav@shesek.info>
2019-03-01 07:03:14 +0200

    l10n: Separate extract-all and update-strings scripts, add l10n:update-strings script, rename fetch-transfix to l10n:fetch

--- lang/util/extract-all.sh
+++ lang/util/extract-all.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-# requires sponge from moreutils
-
-(
-  cat lang/strings.txt
-  for file in client/src/{,**/}*.js; do ./lang/util/extract.js < $file; done
-) | sort | uniq | sponge lang/strings.txt
+for file in client/src/{,**/}*.js; do ./lang/util/extract.js < $file; done
