dce455f418c261e43d7c833c413126d329bbd94d
Nadav Ivgi <nadav@shesek.info>
2019-03-02 07:55:08 +0200

    Add footer link to new mempool page

--- client/src/views/footer.js
+++ client/src/views/footer.js
@@ -12,6 +12,7 @@ export default ({ t, page }) =>
       <div className="footer-logo">
         <div className="footer-links">
           <a href="tx/push">{t`Broadcast tx`}</a> {' | '}
+          <a href="mempool">{t`Mempool`}</a> {' | '}
           { hasCam ? [ <a href="scan-qr">{t`Scan QR`}</a>, ' | '] : '' }
           <a href="https://github.com/Blockstream/esplora/blob/master/API.md">{t`API`}</a>
         </div>
