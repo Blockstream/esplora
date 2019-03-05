9f6040bcb652d1ee5fb48f61341eb657c889c434
Nadav Ivgi <nadav@shesek.info>
2019-03-05 23:08:05 +0200

    Sort estimates prior to searching
    The search in getConfEstimate requires them to sorted
    with the highest fee-rate first.

--- client/src/lib/fees.js
+++ client/src/lib/fees.js
@@ -13,6 +13,7 @@ export function getMempoolDepth(fee_histogram, feerate) {
 // Get the estimated confirmation time (in blocks) for a transaction paying `feerate`
 export function getConfEstimate(fee_estimates, feerate) {
   const target_est = Object.entries(fee_estimates)
+    .sort((a, b) => a[0]-b[0])
     .find(([ target, target_feerate ]) => target_feerate <= feerate)
   return target_est ? target_est[0] : null
 }
