ab5f35978aedb3567d61db4344d4e58b4ef57717
Nadav Ivgi <nadav@shesek.info>
2019-03-05 19:45:02 +0200

    Don't highlight the value column in recent txs table

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -98,7 +98,7 @@ export const recentTxs = ({ mempoolRecent, t, ...S }) => homeLayout(
             <div className="transactions-table-link-row">
               <a className="transactions-table-row transaction-data" href={`tx/${txOverview.txid}`}>
                 <div className="transactions-table-cell highlighted-text" data-label={t`TXID`}>{txOverview.txid}</div>
-                { txOverview.value != null && <div className="transactions-table-cell highlighted-text" data-label={t`Value`}>{formatAmount({ value: txOverview.value })}</div> }
+                { txOverview.value != null && <div className="transactions-table-cell" data-label={t`Value`}>{formatAmount({ value: txOverview.value })}</div> }
                 <div className="transactions-table-cell" data-label={t`Size`}>{`${txOverview.vsize} vB`}</div>
                 <div className="transactions-table-cell" data-label={t`Fee`}>{`${feerate.toFixed(1)} sat/vB`}</div>
               </a>
