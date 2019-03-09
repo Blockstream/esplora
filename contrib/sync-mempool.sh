c2c2d5b14d35118595f97f14d83a49e4083862f6
Nadav Ivgi <nadav@shesek.info>
2019-03-09 14:29:26 +0200

    Add contrib script to sync mempools over http

---
+++ contrib/sync-mempool.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source="$1"
+dest="$2"
+
+curl -s $source/mempool/txids | jq -r .[] | while read txid; do
+  echo pushing $txid
+  curl -s $dest/broadcast?tx=$(curl -s $source/tx/$txid/hex)
+done
