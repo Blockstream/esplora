33bf23a9127439be57a1fe19238de71d07ad7533
Nadav Ivgi <nadav@shesek.info>
2019-03-12 18:29:51 +0200

    Update sync-mempool to use POST

--- contrib/sync-mempool.sh
+++ contrib/sync-mempool.sh
@@ -5,5 +5,6 @@ dest="$2"
 
 curl -s $source/mempool/txids | jq -r .[] | while read txid; do
   echo pushing $txid
-  curl -s $dest/broadcast?tx=$(curl -s $source/tx/$txid/hex)
+  curl -s $dest/tx -d $(curl -s $source/tx/$txid/hex)
+  echo
 done
