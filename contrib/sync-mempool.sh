89fd72677c61725bea23ac84975bfe8bdc28bb5d
Lawrence Nahum <lawrence@greenaddress.it>
2019-04-09 15:43:48 +0200

    only synchronize the difference, randomize the list, fix too big tx issue, run concurrently

--- contrib/sync-mempool.sh
+++ contrib/sync-mempool.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-source="$1"
-dest="$2"
+curl_stream() {
+  hex_tx=$(curl -s $source/tx/$1/hex)
+  curl -s $dest/tx -d @- &>/dev/null <<TXHEX
+  $hex_tx
+TXHEX
+}
 
-curl -s $source/mempool/txids | jq -r .[] | while read txid; do
-  echo pushing $txid
-  curl -s $dest/tx -d $(curl -s $source/tx/$txid/hex)
-  echo
-done
+export -f curl_stream
+
+sync_mempool() {
+  export source="$1"
+  export dest="$2"
+  comm -23 <(curl -s $source/mempool/txids | jq -r .[] | sort) \
+           <(curl -s $dest/mempool/txids | jq -r .[] | sort) \
+    | shuf \
+    | parallel -j40 curl_stream
+  echo "Done $source to $dest"
+}
+
+sync_mempool $1 $2
+sync_mempool $2 $1
+
+echo "All done"
