ab97daa2b8407002a82ae45c7d9aebed679b2361
Nadav Ivgi <nadav@shesek.info>
2019-04-19 01:06:58 +0200

    Avoid creating files

--- contrib/sync-mempool.sh
+++ contrib/sync-mempool.sh
@@ -2,16 +2,6 @@
 export source="$1"
 export dest="$2"
 
-curl -s $source/mempool/txids | jq -r .[] | sort - > tx_ids_src.txt
-curl -s $dest/mempool/txids | jq -r .[] | sort - > tx_ids_dest.txt
-
-
-comm -23 tx_ids_src.txt tx_ids_dest.txt > tx_ids_res.txt
-
-wc -l tx_ids_src.txt
-wc -l tx_ids_dest.txt
-wc -l tx_ids_res.txt
-
 curl_stream() {
   hex_tx=$(curl -s $source/tx/$1/hex)
   curl -s $dest/tx -d @- &>/dev/null <<TXHEX
@@ -23,6 +13,9 @@ TXHEX
 
 export -f curl_stream
 
-shuf tx_ids_res.txt | parallel -j40 curl_stream
+comm -23 <(curl -s $source/mempool/txids | jq -r .[] | sort) \
+         <(curl -s $dest/mempool/txids | jq -r .[] | sort) \
+  | shuf \
+  | parallel -j40 curl_stream
 
 echo "All done"
