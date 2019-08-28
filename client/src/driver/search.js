a2d1447549034523e8d3be3bdbd872dc235b94b2
Nadav Ivgi <nadav@shesek.info>
2019-08-29 02:47:09 +0300

    Support looking up lightning-style short txout identifiers
    Resolves #141

--- client/src/driver/search.js
+++ client/src/driver/search.js
@@ -4,6 +4,7 @@ import { Observable as O } from '../rxjs'
 
 const reNumber  = /^\d+$/
     , reAddr = /^([a-km-zA-HJ-NP-Z1-9]{26,35}|[a-km-zA-HJ-NP-Z1-9]{80}|[a-z]{2,5}1[ac-hj-np-z02-9]{8,87})$/ // very loose regex, might have false positive
+    , reShortTxOut = /^(\d+)([x:])(\d+)\2(\d+)$/
     , trim = s => s.trim()
     , stripUri = s => s.replace(/^bitcoin:([^?]+).*/, '$1')
 
@@ -11,6 +12,8 @@ export default apiBase => {
   const tryResource = path =>
     request(apiBase + path)
       .then(r => r.ok ? path : Promise.reject('invalid status'))
+
+  let matches
 
   // Accepts a stream of query strings, returns a stream of found resource paths
   return query$ =>
@@ -30,6 +33,15 @@ export default apiBase => {
     // lookup as address if it resembles one
     : reAddr.test(query)
     ? tryResource(`/address/${tryUnconfidentialAddress(query)}`)
+        .catch(_ => null)
+
+    // lookup as lightning-style short txout identifier
+    : (matches = query.match(reShortTxOut))
+    ? request(`${apiBase}/block-height/${matches[1]}`)
+        .then(r => r.ok ? r.text : Promise.reject('invalid reply for block height'))
+        .then(blockhash => request(`${apiBase}/block/${blockhash}/txid/${matches[3]}`))
+        .then(r => r.ok ? r.text : Promise.reject('invalid reply for block txid'))
+        .then(txid => `/tx/${txid}?output:${matches[4]}`)
         .catch(_ => null)
 
     // @XXX the tx/block/addr resource will be fetched again later for display,
