fa8077127259e59e199a5947b74fef0b4467ba4d
Chase Sillevis <chase@sillevis.net>
2020-06-18 16:01:47 +0200

    additional things

--- terraform/modules/electrum/firewall.tf
+++ terraform/modules/electrum/firewall.tf
@@ -6,10 +6,10 @@ resource "google_compute_firewall" "all-traffic" {
 
   allow {
     protocol = "tcp"
-    ports    = ["50001", "50002", "50007", "50008", "60001", "60002"]
+    ports    = ["50001", "50002", "50007", "50008", "60001", "60002", "80"]
   }
 
-  source_ranges = ["0.0.0.0/32"]
+  source_ranges = ["0.0.0.0/0"]
 
   target_service_accounts = [
     google_service_account.electrum[0].email,
