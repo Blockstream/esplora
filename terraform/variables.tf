55ce979ca0694aeb891f22f78b59d357432e071d
nitramiz <martin@blockstream.com>
2020-12-18 08:10:20 -0800

    tf: update tor 0.4.3.7

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -128,7 +128,7 @@ variable "docker_tag_explorer" {
 
 variable "docker_tag_tor" {
   type    = string
-  default = "blockstream/tor@sha256:4f99eddb24fb779cc25b43ec0cc1a7a92341b4b9e3d1b02826d0e2ab67360c7f"
+  default = "blockstream/tor@sha256:378aa7ee44452617ba46369e7e27cc89c2704b9d53442cf016543a24e46f984a"
 }
 
 variable "docker_tag_prometheus" {
