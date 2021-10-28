85f11287ea30728134528e14723ae289e41508ef
Adebayo Oriyomi <adebayooriyomi@gmail.com>
2021-10-28 14:27:44 -0700

    Update Favicon for each Network, add Testnet title/widget to liquid-testnet Headings

--- client/src/views/layout.js
+++ client/src/views/layout.js
@@ -2,7 +2,6 @@ import Snabbdom from 'snabbdom-pragma'
 import navbar from './navbar'
 import footer from './footer'
 import subnav from './sub-navbar'
-import widget from './testnet-widget'
 
 export default (body, opt) =>
   <div className="explorer-container">
@@ -11,6 +10,5 @@ export default (body, opt) =>
       {subnav(opt.t, opt.isTouch, opt.activeTab)}
       { body }
     </div>
-    { widget()}
     { footer(opt) }
   </div>
