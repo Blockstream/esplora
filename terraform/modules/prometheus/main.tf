02b79e7ff5b7e95ae21e90e68579855bab4d1d3a
nitramiz <martin@blockstream.com>
2022-03-29 06:25:54 -0700

    TF: fix Prom config to scrape registries

--- terraform/modules/prometheus/main.tf
+++ terraform/modules/prometheus/main.tf
@@ -44,10 +44,10 @@ resource "google_compute_instance" "prometheus-server" {
     email = local.service_account
 
     scopes = [
-      "https://www.googleapis.com/auth/compute.readonly",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/pubsub",
-      "https://www.googleapis.com/auth/logging.write"
+      "compute-rw",
+      "storage-ro",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring.write",
     ]
   }
 
@@ -73,7 +73,6 @@ resource "google_compute_instance" "prometheus-server" {
   }
 
   metadata = {
-    user-data              = data.template_cloudinit_config.prometheus.rendered
-    google-logging-enabled = "true"
+    user-data = data.template_cloudinit_config.prometheus.rendered
   }
 }
