ce7267412a43b1be9f090d2602bcb45b641b913d
Adebayo Oriyomi <adebayooriyomi@gmail.com>
2022-10-24 12:52:55 -0700

    Update Menu: Add jade logo and green logo (#414)
    Remove assets-minimal-json file
    
    Co-authored-by: Adebayo Oriyomi <ade@blockstream.com>

--- client/src/views/nav-toggle.js
+++ client/src/views/nav-toggle.js
@@ -23,11 +23,28 @@ export default (t, theme, page) =>
       <div className="section1">
         <h4 className="menu-title">Wallets</h4>
         <div className="wallets-link">
-            <p>Blockstream Green</p>
-            <div>
-              <a href="https://apps.apple.com/us/app/green-bitcoin-wallet/id1402243590" target="_blank"><img src={`${staticRoot}img/icons/apple.png`} /><span>App Store</span></a>
-              <a href="https://play.google.com/store/apps/details?id=com.greenaddress.greenbits_android_wallet" target="_blank"><img src={`${staticRoot}img/icons/google-play.png`} /><span>Google Play</span></a>
-              <a href="https://github.com/Blockstream/green_qt/releases/download/release_0.1.5/BlockstreamGreen-x86_64.AppImage"><img src={`${staticRoot}img/icons/linux.png`} /><span>Linux</span></a>
+            <div className="wallets-logo">
+              <a href="https://store.blockstream.com/product/blockstream-jade/?utm_source=Blockstream_Explorer&utm_medium=referral&utm_campaign=Blockstream_Explorer_Banner" className="darkmode" target="_blank"><img src={`${staticRoot}img/icons/jade_logo.svg`} alt="jade logo"/></a>
+              <a href="https://blockstream.com/green/" className="darkmode" target="_blank"><img className="green-logo" src={`${staticRoot}img/icons/green_logo.svg`} alt="green logo"/></a>
+              <a href="https://store.blockstream.com/product/blockstream-jade/?utm_source=Blockstream_Explorer&utm_medium=referral&utm_campaign=Blockstream_Explorer_Banner" className="lightmode" target="_blank"><img src={`${staticRoot}img/icons/jade_logo_light.svg`} alt="jade logo"/></a>
+              <a href="https://blockstream.com/green/" className="lightmode" target="_blank"><img className="green-logo" src={`${staticRoot}img/icons/green_logo_light.svg`} alt="green logo"/></a>
+            </div>
+            <div className="store-icons">
+              <a href="https://apps.apple.com/us/app/green-bitcoin-wallet/id1402243590" target="_blank">
+                <img className="darkmode" src={`${staticRoot}img/icons/apple.png`} />
+                <img className="lightmode" src={`${staticRoot}img/icons/apple_dark.png`} />
+                <span>App Store</span>
+              </a>
+              <a href="https://play.google.com/store/apps/details?id=com.greenaddress.greenbits_android_wallet" target="_blank">
+                <img  className="darkmode" src={`${staticRoot}img/icons/google-play.png`} />
+                <img  className="lightmode" src={`${staticRoot}img/icons/google-play_dark.png`} />
+                <span>Google Play</span>
+              </a>
+              <a href="https://github.com/Blockstream/green_qt/releases/download/release_0.1.5/BlockstreamGreen-x86_64.AppImage">
+                <img  className="darkmode" src={`${staticRoot}img/icons/linux.png`} />
+                <img  className="lightmode" src={`${staticRoot}img/icons/linux_dark.png`} />
+                <span>Linux</span>
+              </a>
               <a href="https://blockstream.com/green/" target="_blank">+4 more</a>
             </div>
         </div>
@@ -55,7 +72,6 @@ export default (t, theme, page) =>
             <li><a href="https://blockstream.com/liquid/" target="_blank">Liquid Network</a></li>
             <li><a href="https://blockstream.com/mining/" target="_blank">Blockstream Mining</a></li>
             <li><a href="https://blockstream.com/amp/" target="_blank">Blockstream AMP</a></li>
-            <li><a href="https://blockstream.com/jade/" target="_blank">Blockstream Jade</a></li>
             <li><a href="https://blockstream.com/satellite/" target="_blank">Blockstream Satellite</a></li>
             <li><a href="https://blockstream.com/cryptofeed/" target="_blank">Crypto Data Feed</a></li>
             <li><a href="https://blockstream.com/lightning/" target="_blank">Core Lightning</a></li>
