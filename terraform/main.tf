2fbafdd59b7bce8c4b7ba8040a1f80caeb1314c7
Chase Sillevis <chase@blockstream.com>
2024-10-30 19:55:36 +0100

    terraform: migrated to different solution

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,137 +23,15 @@ provider "google-beta" {
 module "prometheus" {
   source = "./modules/prometheus"
 
-  name                       = "explorer"
-  network                    = "default"
-  zones                      = var.zones
-  region                     = var.regions[0]
-  instances                  = 1
-  machine_type               = var.instance_type
-  retention                  = "2d"
-  project                    = var.project
-  docker_tag                 = var.docker_tag_prometheus
-  docker_tag_node_exporter   = var.docker_tag_node_exporter
-  allowed_source_ip          = var.prometheus_allowed_source_ip
-  prometheus_service_account = terraform.workspace != "main" ? data.terraform_remote_state.main.outputs.prometheus_service_account : ""
-
-  create_resources = local.create_main
-}
-
-module "tor" {
-  source = "./modules/tor"
-
-  name                     = "explorer-tor"
+  name                     = "explorer"
   network                  = "default"
-  zones                    = var.zones[0]
-  region                   = var.regions[0]
+  zone                     = var.zone
+  region                   = var.region
   instances                = 1
+  machine_type             = var.instance_type
+  retention                = "2d"
   project                  = var.project
-  tor_machine_type         = var.instance_type
-  tor_lb                   = element(concat(google_compute_global_address.onion-lb.*.address, tolist([""])), 0)
-  docker_tag               = var.docker_tag_tor
-  hosts_onion              = var.hosts_onion
-  kms_key                  = element(concat(google_kms_crypto_key.esplora-crypto-key.*.name, tolist([""])), 0)
-  kms_key_link             = element(concat(google_kms_crypto_key.esplora-crypto-key.*.id, tolist([""])), 0)
-  kms_key_ring             = element(concat(google_kms_key_ring.esplora-key-ring.*.name, tolist([""])), 0)
-  kms_location             = var.kms_location
-  service_account_prom     = terraform.workspace == "main" ? module.prometheus.service_account : data.terraform_remote_state.main.outputs.prometheus_service_account
+  docker_tag               = var.docker_tag_prometheus
   docker_tag_node_exporter = var.docker_tag_node_exporter
-
-  create_resources = local.create_main
-}
-
-module "bitcoin-testnet" {
-  source = "./modules/daemon"
-
-  regions                     = [var.regions[0]]
-  name                        = "bitcoin-testnet"
-  daemon                      = "bitcoin"
-  mempooldat                  = var.mempooldat
-  fullurl                     = var.fullurl
-  network                     = "testnet"
-  disk_type                   = var.disk_type
-  instance_type               = var.instance_type
-  size                        = var.cluster_size
-  project                     = var.project
-  service_account_prom        = terraform.workspace == "main" ? module.prometheus.service_account : data.terraform_remote_state.main.outputs.prometheus_service_account
-  docker_tag_node_exporter    = var.docker_tag_node_exporter
-  docker_tag_process_exporter = var.docker_tag_process_exporter
-  docker_tag_explorer         = var.docker_tag_explorer
-  min_ready_sec               = var.min_ready_sec
-  initial_delay_sec           = var.initial_delay_sec
-  image_source_project        = var.image_source_project
-
-  create_resources = local.create_bitcoin_testnet
-}
-
-module "bitcoin-mainnet" {
-  source = "./modules/daemon"
-
-  regions                     = var.regions
-  name                        = "bitcoin-mainnet"
-  daemon                      = "bitcoin"
-  network                     = "mainnet"
-  disk_type                   = var.disk_type
-  mempooldat                  = var.mempooldat
-  fullurl                     = var.fullurl
-  instance_type               = var.instance_type
-  size                        = var.cluster_size
-  project                     = var.project
-  service_account_prom        = terraform.workspace == "main" ? module.prometheus.service_account : data.terraform_remote_state.main.outputs.prometheus_service_account
-  docker_tag_node_exporter    = var.docker_tag_node_exporter
-  docker_tag_process_exporter = var.docker_tag_process_exporter
-  docker_tag_explorer         = var.docker_tag_explorer
-  min_ready_sec               = var.min_ready_sec
-  initial_delay_sec           = var.initial_delay_sec
-  image_source_project        = var.image_source_project
-
-  create_resources = local.create_bitcoin_mainnet
-}
-
-module "liquid-mainnet" {
-  source = "./modules/daemon"
-
-  regions                     = [var.regions[0]]
-  name                        = "liquid-mainnet"
-  daemon                      = "liquid"
-  network                     = "mainnet"
-  disk_type                   = var.disk_type
-  mempooldat                  = var.mempooldat
-  fullurl                     = var.fullurl
-  instance_type               = var.instance_type
-  size                        = var.cluster_size
-  project                     = var.project
-  service_account_prom        = terraform.workspace == "main" ? module.prometheus.service_account : data.terraform_remote_state.main.outputs.prometheus_service_account
-  docker_tag_node_exporter    = var.docker_tag_node_exporter
-  docker_tag_process_exporter = var.docker_tag_process_exporter
-  docker_tag_explorer         = var.docker_tag_explorer
-  min_ready_sec               = var.min_ready_sec
-  initial_delay_sec           = var.initial_delay_sec
-  image_source_project        = var.image_source_project
-
-  create_resources = local.create_liquid_mainnet
-}
-
-module "liquid-testnet" {
-  source = "./modules/daemon"
-
-  regions                     = [var.regions[0]]
-  name                        = "liquid-testnet"
-  daemon                      = "liquid"
-  network                     = "testnet"
-  disk_type                   = var.disk_type
-  mempooldat                  = var.mempooldat
-  fullurl                     = var.fullurl
-  instance_type               = var.instance_type
-  size                        = var.cluster_size
-  project                     = var.project
-  service_account_prom        = terraform.workspace == "main" ? module.prometheus.service_account : data.terraform_remote_state.main.outputs.prometheus_service_account
-  docker_tag_node_exporter    = var.docker_tag_node_exporter
-  docker_tag_process_exporter = var.docker_tag_process_exporter
-  docker_tag_explorer         = var.docker_tag_explorer
-  min_ready_sec               = var.min_ready_sec
-  initial_delay_sec           = var.initial_delay_sec
-  image_source_project        = var.image_source_project
-
-  create_resources = local.create_liquid_testnet
+  allowed_source_ip        = var.prometheus_allowed_source_ip
 }
--- terraform/modules/daemon/data.tf
+++
@@ -1,3 +0,0 @@
-data "google_compute_network" "default" {
-  name = "default"
-}
