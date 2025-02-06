3b64921048e3690984797bdcd68e1476a7f231b2
Serhii Prykhozhyi <serhii@blockstream.com>
2025-02-06 09:25:59 -0700

    fix the overflowing style issue and update the generic landing term

--- client/src/views/sub-navbar.js
+++ client/src/views/sub-navbar.js
@@ -9,7 +9,7 @@ export default ( t, isTouch, activeTab) =>
         <a href="blocks/recent" class={{ active: activeTab == 'recentBlocks' }}>Blocks</a>
         <a href="tx/recent" class={{ active: activeTab == 'recentTxs' }}>Transactions</a>
         { process.env.IS_ELEMENTS ? <a href="assets" class={{ active: activeTab == 'assets' }}>Assets<sup className="highlight"></sup></a> : "" }
-        <a href="/explorer-api" class={{ active: activeTab == 'landing' }}>Explorer API</a>
+        <a href="/explorer-api" class={{ active: activeTab == 'apiLanding' }}>Explorer API</a>
       </div>
 
       { search({ t, autofocus: !isTouch }) }
