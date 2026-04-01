f5e6ff04d8f2f0a26b229bf3a8df5e29de7d45a4
Randall Naar <rnd.naar@gmail.com>
2026-04-01 19:35:18 -0400

    Updated navbar to use latest design.

--- client/src/views/navbar-menu.js
+++ client/src/views/navbar-menu.js
@@ -10,15 +10,5 @@ const staticRoot = process.env.STATIC_ROOT || ''
 export default ({ t, theme, page }) =>
 
 <div className="main-nav-container">
-  <ul className="main-nav">
-    { items && Object.entries(items).map(([ name, url ]) =>
-        <li id={name.replace(/ /g, '')} className={`nav-item ${name == active ? 'active' : ''}`}>
-          <a className="nav-link font-h4" href={url} rel="external">
-            <span><img className="menu-logo" alt="" src={`${staticRoot}img/icons/${name.replace(/ /g, '')}-menu-logo.svg`} /></span>
-            <span>{t(name)}</span>
-          </a>
-        </li>
-    ) }
-  </ul>
-    { process.env.NAVBAR_HTML ? navToggle(t, theme, page) : "" }
-  </div>
+  { process.env.NAVBAR_HTML ? navToggle(t, theme, page) : "" }
+</div>
